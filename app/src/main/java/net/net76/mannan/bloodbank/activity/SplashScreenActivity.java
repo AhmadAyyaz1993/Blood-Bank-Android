faa93422815bbfe74e4d70f3e87d1fcd08923a0b
Mannan <mannan541@live.com>
2016-11-04 10:21:50 +0500

    full screen in splash activity.

--- app/src/main/java/net/net76/mannan/bloodbank/activity/SplashScreenActivity.java
+++ app/src/main/java/net/net76/mannan/bloodbank/activity/SplashScreenActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import net.net76.mannan.bloodbank.R;
 
@@ -17,6 +19,11 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
